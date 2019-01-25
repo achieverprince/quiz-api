12f44c86c24d9c1f251fbb240d10ea37e14f5b21
Jeba Prince <achieverprince@gmail.com>
2019-01-25 18:06:26 +0530

    Updates

--- src/main/java/com/achieverprince/quiz/security/jwt/JWTAuthenticationFilter.java
+++ src/main/java/com/achieverprince/quiz/security/jwt/JWTAuthenticationFilter.java
@@ -68,5 +68,6 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(HMAC512(SECRET.getBytes()));
         res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+        res.addHeader("Access-Control-Expose-Headers", "Authorization");
     }
 }
